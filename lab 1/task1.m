5939a3a41b7fcec82e209bf495fe113e1c20b6da
OlexandPeduraru <113430719+OlexandPeduraru@users.noreply.github.com>
2022-09-13 12:42:21 +0300

    Add files via upload

---
+++ lab 1/task1.m
@@ -0,0 +1,21 @@
+disp('Задайте коефіцієнти квадратного рівняння');
+a =input('a=');
+b =input('b=');
+c =input('c=');
+d =b.
+^2-4*a*c;
+
+if b<0
+    disp ('дійсних чисел нема');
+
+elseif d==0
+        x=-b/2/a;
+
+else
+    x1=(-b+sqrt(d))/2/a;
+    x2=(-b-sqrt(d))/2/a;
+fprintf ('x1 =%f',x1);
+fprintf ('\nx2 =%f',x2);
+
+
+end
---
+++ lab 1/task4.m
@@ -0,0 +1,9 @@
+disp('Задайте значення цілих змінних x,y,z');
+x =input('x=');
+y =input('y=');
+z =input('z=');
+t = log2((1-y)/(1+x)+z);
+disp('               1 - y');
+disp('     t =log2 ---------- + z');
+disp('               1 + x')
+fprintf( 'значення функції t : %.2f \n',t); 
