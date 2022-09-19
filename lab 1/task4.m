490af6b3014bb9731ba567fdcb93b26b871e381c
OlexandPeduraru <113430719+OlexandPeduraru@users.noreply.github.com>
2022-09-19 12:03:17 +0300

    Update task4.m

--- lab 1/task4.m
+++ lab 1/task4.m
@@ -3,7 +3,7 @@ x =input('x=');
 y =input('y=');
 z =input('z=');
 t = log2((1-y)/(1+x))+z;
-disp('               (1 - y');
+disp('               1 - y');
 disp('     t =log2 ---------- + z');
-disp('               (1 + x')
+disp('               1 + x')
 fprintf( 'значення функції t : %.2f \n',t); 
